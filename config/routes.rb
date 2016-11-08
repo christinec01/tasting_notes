273b05b1a6875031b3b79901153f9d9f9bd129db
Christine Casebolt <christine.casebolt.gmail.com>
2016-11-07 17:30:38 -0800

    adds bcrypt and user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  get 'notes/new'
+get 'notes/new'
 
  get '/search', to: 'notes#entry'
  root 'notes#new'
+
+get '/login' => 'sessions#new'
+post '/login' => 'sessions#create'
+get '/logout' => 'sessions#destroy'
+
+ get '/signup' => 'users#new'
+ post '/users' => 'users#create'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
